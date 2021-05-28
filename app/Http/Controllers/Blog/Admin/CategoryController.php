ee87421d56416d0d3d09d65ab2f0d2f38872863b
Valiusia <imfany2003@gmail.com>
2021-05-28 19:45:27 +0300

    Lab5

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Blog\Admin;
 //use App\Http\Controllers\Controller;
 use App\Models\BlogCategory;
 use Illuminate\Support\Str;
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
+use App\Http\Requests\BlogCategoryUpdateRequest;
+use App\Http\Requests\BlogCategoryCreateRequest;
 
 class CategoryController extends BaseController
 {
@@ -30,6 +32,10 @@ class CategoryController extends BaseController
     public function create()
     {
         //dd(__METHOD__);
+        $item = new BlogCategory();
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
     }
 
     /**
@@ -38,9 +44,25 @@ class CategoryController extends BaseController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BlogCategoryCreateRequest $request)
     {
         //dd(__METHOD__);
+        $data = $request->input(); //отримаємо масив даних, які надійшли з форми
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+
+        $item = (new BlogCategory())->create($data); //створюємо об'єкт і додаємо в БД
+
+        if ($item) {
+            return redirect()
+                ->route('blog.admin.categories.edit', [$item->id])
+                ->with(['success' => 'Успішно збережено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
     }
 
     /**
@@ -76,7 +98,7 @@ class CategoryController extends BaseController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(BlogCategoryUpdateRequest $request, $id)
     {
         //dd(__METHOD__);
         $item = BlogCategory::find($id);
@@ -102,6 +124,28 @@ class CategoryController extends BaseController
                 ->with(['msg' => 'Помилка збереження'])
                 ->withInput();
         }
+        $rules = [
+            'title' => 'required|min:5|max:200',
+            'slug' => 'max:200',
+            'description' => 'string|max:500|min:3',
+            'parent_id' => 'required|integer|exists:blog_categories,id',
+        ];
+
+
+        $validatedData = $this->validate($request, $rules);  //валідація в контроллері
+
+
+        $validatedData = $request->validate($rules);   // валідація через об'єкт реквест
+
+
+        $validator = Validator::make($request->all(), $rules);  //валідація через клас
+        $validatedData[] = $validator->passes(); //перевіряє чи все ок
+        $validatedData[] = $validator->validate(); //редірект якщо помилка
+        $validatedData[] = $validator->valid(); //видає валідні дані
+        $validatedData[] = $validator->failed(); //видає невалідні дані
+        $validatedData[] = $validator->errors(); //текст помилки
+        $validatedData[] = $validator->fails(); //1, якщо помилка
+
     }
 
     /**
